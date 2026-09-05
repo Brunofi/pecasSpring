8ceacb8159dcbac04a00286a90efa842ae1a521c
Bruno Sousa <Bruno@porschegt3cup.com.br>
2026-09-05 08:16:08 -0300

    ajuste de tabelas

--- src/main/java/com/pecassystem/pecas/repositorio/EstoqueRepositorio.java
+++ src/main/java/com/pecassystem/pecas/repositorio/EstoqueRepositorio.java
@@ -11,11 +11,11 @@ import java.util.List;
 public interface EstoqueRepositorio extends CrudRepository<Estoque, Integer> {
 
     // Busca estoques pelo part number da peça
-    @Query("SELECT e FROM Estoque e JOIN e.peca p WHERE p.partnumber LIKE %:partnumber%")
+    @Query("SELECT e FROM Estoque e JOIN FETCH e.peca p JOIN FETCH e.locacao l WHERE p.partnumber LIKE %:partnumber%")
     List<Estoque> findByPartNumber(@Param("partnumber") String partnumber);
 
     // Busca estoques pelo nome da peça
-    @Query("SELECT e FROM Estoque e WHERE e.peca.nome LIKE %:nome%")
+    @Query("SELECT e FROM Estoque e JOIN FETCH e.peca p JOIN FETCH e.locacao l WHERE p.nome LIKE %:nome%")
     List<Estoque> findByNomePeca(@Param("nome") String nome);
 
     // Busca estoques pelo id da peça
